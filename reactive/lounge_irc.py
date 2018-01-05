2e28867a0aaf83e60a1ce7532dce60bc8953d600
jamesbeedy <jamesbeedy@gmail.com>
2018-01-05 13:28:57 -0800

    provisioning via snap

--- reactive/lounge_irc.py
+++ reactive/lounge_irc.py
@@ -1,94 +1,91 @@
 import os
 import subprocess
 
-from charms.reactive import when, when_not, set_state, hook, remove_state
+from charms.reactive import (
+    endpoint_from_flag,
+    hook,
+    remove_state,
+    set_flag,
+    when,
+    when_not,
+)
 
 from charmhelpers.core import host
 
-from charmhelpers.core.hookenv import config
-from charmhelpers.core.hookenv import status_set
-from charmhelpers.core.hookenv import open_port
-from charmhelpers.core.hookenv import close_port
+from charmhelpers.core.hookenv import (
+    application_version_set,
+    config,
+    status_set,
+    open_port,
+    close_port,
+)
 
 from charmhelpers.core.templating import render
 
-from charms.layer.nodejs import npm, node_dist_dir
+
+LOUNGE_HOME = os.path.join(
+    '/', 'var', 'snap', 'thelounge', 'common', 'etc', 'thelounge')
 
 
-@when('nodejs.available')
-@when_not('lounge-irc.installed')
-def install_lounge():
-    status_set('maintenance', 'installing lounge-irc')
-    npm('-g', 'install', 'thelounge')
+@when('snap.installed.thelounge')
+@when_not('thelounge.available')
+def configure_thelounge():
+    status_set('maintenance', 'Configuring thelounge irc')
 
     render(
         source='config.js.j2',
-        target=os.path.join(node_dist_dir(), 'config.js'),
+        target=os.path.join(LOUNGE_HOME, 'config.js'),
         context={
             'public': 'true' if config('public') else 'false',
             'port': config('port')
         },
     )
 
-    set_state('lounge-irc.installed')
-    remove_state('lounge-irc.started')
-    status_set('active', 'lounge-irc installed')
-
-
-@when('website.available')
-@when('lounge-irc.installed')
-def configure_website(website):
-    website.configure(port=config('port'))
-
-
-@when('lounge-irc.installed')
-@when_not('lounge-irc.started')
-def start():
     render(
-        source='lounge-irc.service.j2',
-        target='/etc/systemd/system/lounge-irc.service',
-        context={
-            'home': node_dist_dir(),
-        },
+        source='_.json',
+        target=os.path.join(LOUNGE_HOME, 'users', '_.json'),
+        context={}
     )
 
-    if not config('public'):
-        users_dir = os.path.join(node_dist_dir(), 'users')
-        if not os.path.exists(users_dir):
-            os.mkdir(users_dir)
-
-        render(source='_.json', target=os.path.join(users_dir, '_.json'), context={})
-
-    if host.service_running('lounge-irc'):
-        host.service_restart('lounge-irc')
+    if host.service_running('snap.thelounge.thelounge'):
+        host.service_restart('snap.thelounge.thelounge')
     else:
-        host.service_start('lounge-irc')
+        host.service_start('snap.thelounge.thelounge')
 
-    set_state('lounge-irc.started')
     update()
 
+    set_flag('thelounge.available')
+    status_set('active', 'thelounge configured')
 
-@when('lounge-irc.started')
-def ports():
+
+@when('endpoint.http.joined')
+@when('thelounge.available')
+def configure_website():
+    endpoint = endpoint_from_flag('endpoint.http.joined')
+    endpoint.configure(port=config('port'))
+
+
+@when('thelounge.available')
+def open_lounge_port():
     open_port(config('port'))
 
 
-@when_not('lounge-irc.started')
-def close():
+@when_not('thelounge.available')
+def close_lounge_port():
     close_port(config('port'))
 
 
 @hook('update-status')
 def update():
-    extra = 'running' if host.service_running('lounge-irc') else 'stopped'
+    extra = 'running' if host.service_running('snap.thelounge.thelounge') else 'stopped'
     status_set('active', 'version %s - %s' % (lounge_version(), extra))
 
 
-@when_not('nodejs.available')
-def reset():
-    remove_state('lounge-irc.installed')
+@hook('upgrade-charm')
+def set_revision_as_version():
+    application_version_set(lounge_version())
 
 
 def lounge_version():
-    return subprocess.check_output(['lounge', '--version']).decode('UTF-8').strip()
-
+    return subprocess.check_output(
+        ['thelounge.thelounge-cli', '--version']).decode('UTF-8').strip()
